037d2e07af77c3c54c932b4754272cbe2f3c2578
bender <kudin.dima@gmail.com>
2026-05-21 12:48:56 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,6 @@ export default function LandingPage() {
       navItems={[
         { name: "Services", id: "/services" },
         { name: "How It Works", id: "/how-it-works" },
-        { name: "Shipment Specs", id: "#specs" },
         { name: "Guarantee", id: "/the-sir-james-transit-guarantee" },
         { name: "Contact", id: "/contact" },
       ]}

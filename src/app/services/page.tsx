34d2d8e2d30131d60c3204720c1ba6da50ae0888
bender <kudin.dima@gmail.com>
2026-05-21 21:51:46 +0000

    Update src/app/services/page.tsx

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -11,6 +11,7 @@ export default function ServicesPage() {
     { name: "Services", id: "/services" },
     { name: "How It Works", id: "/how-it-works" },
     { name: "Guarantee", id: "/the-sir-james-transit-guarantee" },
+    { name: "Gallery", id: "/gallery" },
     { name: "Contact", id: "/contact" },
   ];
 

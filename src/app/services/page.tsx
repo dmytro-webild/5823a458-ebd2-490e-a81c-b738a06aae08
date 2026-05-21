015ab3cfd0d455a997f9d0030008cd5d81ab3b75
bender <kudin.dima@gmail.com>
2026-05-21 12:59:38 +0000

    Switch to version 5: modified src/app/services/page.tsx

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -26,7 +26,7 @@ export default function ServicesPage() {
             navItems={[
               { name: "Services", id: "/services" },
               { name: "How It Works", id: "/how-it-works" },
-              { name: "Guarantee", id: "/the-sir-james-transit-guarantee" },
+              { name: "Shipment Specs", id: "/#specs" },
               { name: "Contact", id: "/contact" },
             ]}
             brandName="Sir James Delivery"

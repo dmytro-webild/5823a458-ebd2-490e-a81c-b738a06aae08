dc8f4a0a81fe64399c414d24d68cd2a1ad11dd87
bender <kudin.dima@gmail.com>
2026-05-23 09:16:20 +0000

    Update src/app/the-sir-james-transit-guarantee/page.tsx

--- src/app/the-sir-james-transit-guarantee/page.tsx
+++ src/app/the-sir-james-transit-guarantee/page.tsx
@@ -12,6 +12,7 @@ export default function TransitGuaranteePage() {
     { name: "How It Works", id: "/how-it-works" },
     { name: "Guarantee", id: "/the-sir-james-transit-guarantee" },
     { name: "Gallery", id: "/gallery" },
+    { name: "Loading Gallery", id: "/loading-gallery" },
     { name: "Contact", id: "/contact" },
   ];
 
